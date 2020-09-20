770ba4626542b75aa285a5b3ce4e96a31c92753d
WangY <wangy@outlook.com>
2020-09-21 07:06:25 +0800

    整合entity   服务中心

--- cloud-cosumer/src/main/java/com/ever/cosumer/CosumerMain.java
+++ cloud-cosumer/src/main/java/com/ever/cosumer/CosumerMain.java
@@ -3,9 +3,10 @@ package com.ever.cosumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CosumerMain{
     public static void main(String[] args) {
         SpringApplication.run(CosumerMain.class,args);
--- cloud-payment/src/main/java/com/ever/payment/PaymentMain.java
+++ cloud-payment/src/main/java/com/ever/payment/PaymentMain.java
@@ -2,6 +2,7 @@ package com.ever.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author WangY
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/9/1714:48
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain {
 
     public static void main(String[] args) {

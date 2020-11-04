9b6e49245728bb2f5b6ce0ec0c8065e6a9669b7a
WangY <wangy0o@ouutlook.com>
2020-11-04 16:57:15 +0800

    stream 开始

---
+++ cloud-stream-rabbitmq-consumer/src/main/java/com/ever/springcloud/ConsumerStreamMQMain.java
@@ -0,0 +1,18 @@
+package com.ever.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WangY
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/11/49:06
+ */
+@SpringBootApplication
+public class ConsumerStreamMQMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerStreamMQMain.class,args);
+    }
+}
--- cloud-stream-rabbitmq-provider/src/main/java/com/ever/springcloud/service/impl/MessageProviderImpl.java
+++ cloud-stream-rabbitmq-provider/src/main/java/com/ever/springcloud/service/impl/MessageProviderImpl.java
@@ -1,6 +1,7 @@
 package com.ever.springcloud.service.impl;
 
 import com.ever.springcloud.service.IMessageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.MessageChannel;
@@ -19,12 +20,14 @@ import java.util.UUID;
 @EnableBinding(Source.class)
 public class MessageProviderImpl implements IMessageProvider {
 
+    @Autowired
     private MessageChannel output;
 
     @Override
     public String send() {
         String serial = UUID.randomUUID().toString();
         output.send(MessageBuilder.withPayload(serial).build());
+        System.out.println("****serial : " + serial);
         return null;
     }
 }

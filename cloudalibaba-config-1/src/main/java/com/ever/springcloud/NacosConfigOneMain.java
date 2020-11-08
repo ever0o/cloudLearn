b07f44932083618df8dad8a9f14dd259b174cedb
WangY <wangy@outlook.com>
2020-11-08 23:56:54 +0800

    继续stream

---
+++ cloudalibaba-config-1/src/main/java/com/ever/springcloud/NacosConfigOneMain.java
@@ -0,0 +1,14 @@
+package com.ever.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigOneMain {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigOneMain.class,args);
+    }
+}
+
